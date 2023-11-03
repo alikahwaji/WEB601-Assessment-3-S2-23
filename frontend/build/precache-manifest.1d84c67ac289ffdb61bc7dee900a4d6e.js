48c6cfda72c9a567f456e7eb0b590e899a52efe1
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-11-03 13:21:52 +1300

    files for ecommerce application moved

---
+++ frontend/build/precache-manifest.1d84c67ac289ffdb61bc7dee900a4d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56d1514b185c0e4e0f1989e56510a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48ad5a92db77034cd168",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ad5a92db77034cd168",
+    "url": "/static/js/main.d09e7df9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
